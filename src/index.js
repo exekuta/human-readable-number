9ae4fda4c74c589a4ac783458e9524862d2bcdab
Exekuta <exekuta@gmail.com>
2021-08-18 11:52:04 +0300

    add some code

--- src/index.js
+++ src/index.js
@@ -5,23 +5,27 @@ module.exports = function toReadable (number) {
 
     let zeroItems = number.toString().charAt(2) == 0 ? '' : ' ' + items[number%10];
     let teenItems = (number % 100 >= 11 && number % 100 <= 19);
-    let dozenItems = teenItems ? teens[number.toString().charAt(2)] : dozens[number.toString().charAt(1)];
+    let dozenItems = teenItems ? ' ' + teens[number.toString().charAt(2)] : ' ' + dozens[number.toString().charAt(1)];
 
     if (number.toString().length === 1) {
         return items[number];
-    };
+    }
 
     if (number.toString().length === 2 && teenItems) {
         return teens[number.toString().charAt(1)];
-    };
+    }
 
     if (number.toString().length === 2) {
         return dozens[number.toString().charAt(1)];
     }
 
+    if (number.toString().length === 3 && teenItems) {
+        return items[Math.trunc(number/100)] + ' hundred' + dozenItems;
+    }
+    
     if (number.toString().length === 3) {
-        return items[Math.trunc(number/100)] + ' hundred ' + dozenItems + zeroItems;
-    };
+        return items[Math.trunc(number/100)] + ' hundred' + dozenItems + zeroItems;
+    }
 };
-// number = 40;
+// number = 80;
 // console.log(toReadable(number));
